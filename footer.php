5de49d2d25d65500ee35f74efa3b6417ec190d1d
Hariom1001 <94869944+Hariom1001@users.noreply.github.com>
2022-08-12 12:30:28 +0530

    fdf
    dfd

---
+++ footer.php
@@ -0,0 +1,67 @@
+<div class="right-slide ">
+
+<a href="https://freefonticon.xyz/"><img src="../media/img/freefont-ad1.png" alt=""></a>
+
+</div><br><br>
+<footer>
+        <!-- ..................follow us.......................  -->
+<div>
+   <h3>Follow Us</h3>
+   <ul>
+       <li> <i class="fab fa-facebook"></i><a href="https://www.facebook.com/profile.php?id=100075372720249"> Facebook</a></li>
+       <li><i class="fab fa-instagram"></i> <a href="https://www.instagram.com/coding_seekho_official/"> Instagram</a></li>
+       <li><i class="fab fa-linkedin-in"></i> <a href="https://www.linkedin.com/in/hariom-sah-b527141ba/"> LinkedIn</a></li>
+       <li><i class="fab fa-twitter"></i> <a href="https://twitter.com/CodingSeekho"> Twitter</a></li>
+       <li><i class="fab fa-pinterest"></i> <a href="https://in.pinterest.com/business/hub"> Printerest</a></li>
+       <li><i class="fab fa-youtube"></i> <a href="https://www.youtube.com/channel/UCi341CTiNxlqUoYGdqwW8eQ"> YouTube</a></li>
+   </ul>
+</div>
+<!-- ..............projects................  -->
+<div>
+    <h3>Practice Projects</h3>
+    <ul>
+        <li>Landing Pages</li>
+        <li>User Forms</li>
+        <li>Stylish Buttons</li>
+        <li>Slider</li>
+        <li>Navigation Bar</li>
+        
+    </ul>
+</div>
+<!-- ..............About Website......................  -->
+<div><h3>About Website</h3>
+
+    <ul>
+        <li> <i class="fas fa-address-card"></i> <a href=""> About Us</a></li>
+        <li><i class="far fa-address-book"></i>  <a href=""> Contact Us</a></li>
+        <li><i class="fad fa-exclamation-triangle"></i> <a href=""> Disclaimer</a></li>
+        <li><i class="fas fa-eye"></i> <a href=""> Privacy Policy</a></li>
+   
+    </ul>
+
+
+</div>
+
+<!-- ..............Our Services...................... -->
+<!-- <div>
+    <h3>Our Services</h3>
+
+    <ul>
+        <li>Tutorials</li>
+        <li>Buy Hosting</li>
+        <li>E-books</li>
+        <li>Hire Me</li>
+        <li>Source Code</li>
+    </ul>
+
+
+</div> -->
+
+    </footer>
+   
+<div class="sub-footer">All 2022 Copyright reserved by CodingSeekho</div>
+</div>
+   
+   </body>
+   
+   </html>
---
+++ navbar.php
@@ -0,0 +1,98 @@
+
+
+
+
+  <div class="upper-sm-nav">
+    <div class="sm-nav">
+<div >
+        <a href="https://codingseekho.com/">  <span class="home-icon" style="color: dodgerblue;">  <i class="ffi-house-fill"></i></span>       </a>
+      </div>
+
+      <div class="menu-icon">
+        <span class="bar1"></span>
+        <span class="bar2"></span>
+        <span class="bar3"></span>
+      </div>
+      
+
+    </div>
+  </div>
+  <?php
+
+  // include_once "side-bar.php";
+
+  ?>
+ 
+  <div class="main-upper-nav">
+    <div class="main-head">
+
+      <!-- Text-logo  -->
+
+      <div class="text-logo" style="background-color: #fff; padding: 6px;font-size: 1.4rem;letter-spacing: 1.5px;
+      font-family: 'Mouse Memoirs', sans-serif;
+      ">          
+       <a href="./index.php"> <div style="border-bottom: 2px solid dodgerblue;font-weight: bolder;    padding: 3px 0px;" >  <span style="background-color: dodgerblue;color:#fff;padding: 3px 5px; font-weight: 600px;"><i class="ffi-code-solid"></i> Coding</span><span style="color:#000;"> Seekho</span></div> </a>
+    <!-- <img src="../media/img/codingseekho-logo.png" alt="">-->
+
+      </div>
+  <ul class="main-ul">
+      <li><a href="#" class="nav-link1">     <i class="ffi-house-fill"></i>       Home</a></li>
+      <li><a href="./courses.php" class="nav-link1 courses">     <i class="ffi-play-circle"></i>        Courses 
+      
+    <!-- <ul class="sub-ul">
+      <li><a href="" class="nav-link2">HTML</a></li>
+      <li><a href="" class="nav-link2">CSS</a></li>
+      <li><a href="" class="nav-link2">PHP</a></li>
+      <li><a href="" class="nav-link2">Javascript</a></li>
+    </ul> -->
+    
+    
+    </a></li>
+     <li> <a href="../project.php" class="nav-link1">     <i class="ffi-list-stars"></i>       Project </a></li>
+      <li><a href="https://blog.codingseekho.com/" class="nav-link1">     <i class="ffi-read-book-alt"></i>       Blog</a></li>
+      <li><a href="./how-to.php" class="nav-link1">     <i class="ffi-patch-question-fill"></i>       How To</a></li>
+      <li><a href="" class="nav-link1">    <i class="ffi-ebook"></i>       e-Book</a></li>
+      <li><a href="" class="nav-link1">     <i class="ffi-question-square"></i>       Interview Ouestions</a></li>
+    </ul>
+      <!-- Search box  -->
+     <div></div>
+     <div></div>
+     <div></div>
+      <div >
+        <!-- <form action="../show-result.php" method="post">
+        <input type="search" placeholder="Search Now...." name="get_search_ques" /><input type="submit" value="Search" name="submit" />
+</form> -->
+<!-- <script async src="https://cse.google.com/cse.js?cx=a0b50b09e9b9ce5bd">
+</script>
+<div class="gcse-search"></div> -->
+      </div>
+    </div>
+  </div>
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJC2SBT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "CodingSeekho",
+  "alternateName": "Coding Seekho",
+  "url": "https://www.codingseekho.com/",
+  "logo": "https://www.codingseekho.com/media/img/codingseekho-logo.png",
+  "sameAs": [
+    "https://www.facebook.com/CodingSeekho/",
+    "https://twitter.com/CodingSeekho",
+    "https://www.instagram.com/coding_seekho_official/",
+    "https://www.youtube.com/channel/UCi341CTiNxlqUoYGdqwW8eQ",
+    "https://www.linkedin.com/in/hariom-sah-b527141ba/",
+    "https://in.pinterest.com/hariomsah1001/_saved/",
+    "https://www.codingseekho.com/"
+  ]
+}
+</script>
+  <script src="../myjquery.js"></script>
+
+  
